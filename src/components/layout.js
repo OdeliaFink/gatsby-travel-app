989cefb0b12bdb54e50f15ca2b34f69cb73c369c
OdeliaFink <odelia.finkel@gmail.com>
2022-01-25 14:46:54 -0500

    Added global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 // import PropTypes from "prop-types"
 import Header from "./Header"
+import { GlobalStyle } from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
     </>
